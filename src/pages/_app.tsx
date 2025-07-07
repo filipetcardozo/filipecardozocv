4e362f23c82be09a7307cbb0de2eccfc4be7f00e
Filipe <filipetcardozo@gmail.com>
2025-07-07 15:34:07 -0300

    feat: add Hotjar tracking script to the application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,30 @@ import '@/styles/globals.css';
 import 'highlight.js/styles/atom-one-dark.css';
 import type { AppProps } from 'next/app';
 import { ThemeModeProvider } from '@/contexts/ThemeModeContext';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeModeProvider>
-      <Component {...pageProps} />
-    </ThemeModeProvider>
+    <>
+      <Script
+        id="hotjar"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:6457053,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+          `,
+        }}
+      />
+      <ThemeModeProvider>
+        <Component {...pageProps} />
+      </ThemeModeProvider>
+    </>
   );
 }
